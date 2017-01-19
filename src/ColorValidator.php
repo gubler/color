4774a1204e37269270fa6449b7fe85e832039bed
Daryl Gubler <daryl@dev88.co>
2017-01-18 21:34:49 -0500

    Added tests to ColorValidator

--- src/ColorValidator.php
+++ src/ColorValidator.php
@@ -27,19 +27,32 @@ class ColorValidator
     const RGB_REGEX = '/^rgb\(\s*\d{1,3},\s*\d{1,3},\s*\d{1,3}\)$/';
     const RGBA_REGEX = '/^rgba\(\s*\d{1,3},\s*\d{1,3},\s*\d{1,3},\s*(0|1|0\.[1-9]+)\s*\)$/';
 
+    /**
+     * @param string $color
+     *
+     * @return bool
+     * @throws InvalidColorException
+     */
     public static function check(string $color)
     {
-        if (substr($color, 0, 5) === 'rgba(') {
-            return self::rgba($color);
-        } elseif (substr($color, 0, 4) === 'rgb(') {
-            return self::rgb($color);
-        } elseif (substr($color, 0, 1) === '#' || strlen($color) === 6) {
-            return self::hex($color);
+        if (
+            preg_match(self::RGB_REGEX, $color) ||
+            preg_match(self::RGBA_REGEX, $color) ||
+            preg_match(self::SHORT_HEX_REGEX, $color) ||
+            preg_match(self::LONG_HEX_REGEX, $color)
+        ) {
+            return true;
         }
 
         throw new InvalidColorException('Invalid Color. `'.$color.'` provided.');
     }
 
+    /**
+     * @param string $hex
+     *
+     * @return bool
+     * @throws InvalidHexChannelException
+     */
     public static function hexChannel(string $hex)
     {
         if (preg_match(self::HEX_CHANNEL_REGEX, $hex) === 0) {
@@ -49,6 +62,12 @@ class ColorValidator
         return true;
     }
 
+    /**
+     * @param int $rgb
+     *
+     * @return bool
+     * @throws InvalidRgbChannelException
+     */
     public static function rgbChannel(int $rgb)
     {
         if ($rgb < self::RGB_CHANNEL_MIN || $rgb > self::RGB_CHANNEL_MAX) {
@@ -58,6 +77,12 @@ class ColorValidator
         return true;
     }
 
+    /**
+     * @param float $alpha
+     *
+     * @return bool
+     * @throws InvalidAlphaChannelException
+     */
     public static function alphaChannel(float $alpha)
     {
         if ($alpha < self::ALPHA_CHANNEL_MIN || $alpha > self::ALPHA_CHANNEL_MAX) {
@@ -67,6 +92,14 @@ class ColorValidator
         return true;
     }
 
+    /**
+     * Validate hex color string
+     *
+     * @param string $hex
+     *
+     * @return bool
+     * @throws InvalidHexColorException
+     */
     public static function hex(string $hex)
     {
         if (preg_match(self::LONG_HEX_REGEX, $hex) === 0) {
@@ -76,6 +109,14 @@ class ColorValidator
         return true;
     }
 
+    /**
+     * Validate short hex color string
+     *
+     * @param string $hex
+     *
+     * @return bool
+     * @throws InvalidHexColorException
+     */
     public static function shortHex(string $hex)
     {
         if (preg_match(self::SHORT_HEX_REGEX, $hex) === 0) {
@@ -85,6 +126,14 @@ class ColorValidator
         return true;
     }
 
+    /**
+     * Validate RGB string
+     *
+     * @param string $rgb
+     *
+     * @return bool
+     * @throws InvalidRgbColorException
+     */
     public static function rgb(string $rgb)
     {
         if (preg_match(self::RGB_REGEX, $rgb) === 0) {
@@ -94,6 +143,14 @@ class ColorValidator
         return true;
     }
 
+    /**
+     * Validate RGBA string
+     *
+     * @param string $rgba
+     *
+     * @return bool
+     * @throws InvalidRgbaColorException
+     */
     public static function rgba(string $rgba)
     {
         if (preg_match(self::RGBA_REGEX, $rgba) === 0) {
--- tests/ColorValidatorTest.php
+++ tests/ColorValidatorTest.php
@@ -12,8 +12,155 @@ use Gubler\Color\ColorValidator;
 class ColorValidatorTest extends \PHPUnit_Framework_TestCase
 {
     /** @test */
-    public function not_implemented()
+    public function check_matches_valid_colors()
     {
-        self::markTestIncomplete();
+        $validator = new ColorValidator();
+
+        self::assertTrue($validator->check('rgb(1,1,1)'));
+        self::assertTrue($validator->check('rgba(1,1,1,1)'));
+        self::assertTrue($validator->check('#111111'));
+        self::assertTrue($validator->check('#111'));
+
+        self::assertTrue(ColorValidator::check('rgb(1,1,1)'));
+        self::assertTrue(ColorValidator::check('rgba(1,1,1,1)'));
+        self::assertTrue(ColorValidator::check('#111111'));
+        self::assertTrue(ColorValidator::check('#111'));
+    }
+
+    /**
+     * @test
+     * @expectedException Gubler\Color\Exception\InvalidColorException
+     */
+    public function check_errors_invalid_colors()
+    {
+        $validator = new ColorValidator();
+        $validator->check('moo');
+    }
+
+    /** @test */
+    public function hex_channel_matches_hex_value()
+    {
+        $validator = new ColorValidator();
+        self::assertTrue($validator->hexChannel('FF'));
+        self::assertTrue(ColorValidator::hexChannel('11'));
+    }
+
+    /**
+     * @test
+     * @expectedException Gubler\Color\Exception\InvalidHexChannelException
+     */
+    public function hex_channel_errors_invalid_hex_value()
+    {
+        $validator = new ColorValidator();
+        $validator->hexChannel('moo');
+    }
+
+    /** @test */
+    public function rgb_channel_matches_rgb_value()
+    {
+        $validator = new ColorValidator();
+        self::assertTrue($validator->rgbChannel(100));
+        self::assertTrue(ColorValidator::rgbChannel(233));
+    }
+
+    /**
+     * @test
+     * @expectedException Gubler\Color\Exception\InvalidRgbChannelException
+     */
+    public function rgb_channel_errors_invalid_rgb_value()
+    {
+        $validator = new ColorValidator();
+        $validator->rgbChannel(256);
+    }
+
+    /** @test */
+    public function alpha_channel_matches_alpha_value()
+    {
+        $validator = new ColorValidator();
+        self::assertTrue($validator->alphaChannel(1));
+        self::assertTrue($validator->alphaChannel(0));
+        self::assertTrue(ColorValidator::alphaChannel(0.5));
+    }
+
+    /**
+     * @test
+     * @expectedException Gubler\Color\Exception\InvalidAlphaChannelException
+     */
+    public function alpha_channel_errors_invalid_alpha_value()
+    {
+        $validator = new ColorValidator();
+        $validator->alphaChannel(1.1);
+    }
+
+    /** @test */
+    public function hex_matches_hex_color()
+    {
+        $validator = new ColorValidator();
+        self::assertTrue($validator->hex('#ffffff'));
+        self::assertTrue(ColorValidator::hex('#1A1E00'));
+    }
+
+    /**
+     * @test
+     * @expectedException Gubler\Color\Exception\InvalidHexColorException
+     */
+    public function hex_errors_invalid_hex_color()
+    {
+        $validator = new ColorValidator();
+        $validator->hex('#FFF');
+    }
+
+    /** @test */
+    public function short_hex_matches_short_hex_color()
+    {
+        $validator = new ColorValidator();
+        self::assertTrue($validator->shortHex('#fff'));
+        self::assertTrue(ColorValidator::shortHex('#1A1'));
+    }
+
+    /**
+     * @test
+     * @expectedException Gubler\Color\Exception\InvalidHexColorException
+     */
+    public function short_hex_errors_invalid_short_hex_color()
+    {
+        $validator = new ColorValidator();
+        $validator->shortHex('#FFFfff');
+    }
+
+    /** @test */
+    public function rgb_matches_rgb_color()
+    {
+        $validator = new ColorValidator();
+        self::assertTrue($validator->rgb('rgb(1,1,1)'));
+        self::assertTrue(ColorValidator::rgb('rgb(0,255,0)'));
+    }
+
+    /**
+     * @test
+     * @expectedException Gubler\Color\Exception\InvalidRgbColorException
+     */
+    public function rgb_errors_invalid_rgb_color()
+    {
+        $validator = new ColorValidator();
+        $validator->rgb('rgb(0,0,1000)');
+    }
+
+    /** @test */
+    public function rgba_matches_rgba_color()
+    {
+        $validator = new ColorValidator();
+        self::assertTrue($validator->rgba('rgba(1,1,1,0.2)'));
+        self::assertTrue(ColorValidator::rgba('rgba(0,255,0,1)'));
+    }
+
+    /**
+     * @test
+     * @expectedException Gubler\Color\Exception\InvalidRgbaColorException
+     */
+    public function rgba_errors_invalid_rgba_color()
+    {
+        $validator = new ColorValidator();
+        $validator->rgba('rgba(0,0,100,3)');
     }
 }
